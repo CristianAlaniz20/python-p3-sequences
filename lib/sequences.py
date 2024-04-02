b02efcf8612186863da2088b3ca73295e28b2250
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-04-02 00:27:52 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        empty_list = []
+        print(empty_list)
+    elif length == 1:
+        new_list = [0]
+        print(new_list)
+    elif length == 2:
+        print([0, 1])
+    elif length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
